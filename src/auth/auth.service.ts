5b6ac0658fe921ac7c2f04a38b2d467d45dfa01e
Lalit Rajak <lalitrajak312@gmail.com>
2025-08-14 23:41:09 +0530

    added login logic using webauthn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,16 +2,20 @@ import {
   AuthenticatorTransportFuture,
   generateRegistrationOptions,
   PublicKeyCredentialCreationOptionsJSON,
+  PublicKeyCredentialRequestOptionsJSON,
   RegistrationResponseJSON,
   verifyRegistrationResponse,
+  generateAuthenticationOptions,
+  verifyAuthenticationResponse,
+  AuthenticationResponseJSON,
 } from '@simplewebauthn/server';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ChallengeType } from '@prisma/client';
 
-const rpName = 'My NestJS App';
+const rpName = process.env.RP_NAME || 'My NestJS App';
 const rpID = process.env.RP_ID || 'localhost';
-const origin = process.env.ORIGIN || 'http://localhost:3000';
+const origin = process.env.WEB_APP_ORIGIN || 'http://localhost:3000';
 
 @Injectable()
 export class AuthService {
@@ -100,5 +104,93 @@ export class AuthService {
       },
     });
     return verified;
+  }
+
+  async generateAuthenticationOptions(username: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { username },
+      include: { passkeys: true },
+    });
+
+    if (!user) return null;
+
+    const options: PublicKeyCredentialRequestOptionsJSON =
+      await generateAuthenticationOptions({
+        rpID,
+        allowCredentials: user.passkeys.map((passkey) => ({
+          id: passkey.id,
+          transports: passkey.transports as AuthenticatorTransportFuture[],
+        })),
+      });
+
+    await this.prisma.webAuthnChallenge.create({
+      data: {
+        userId: user.id,
+        type: 'AUTHENTICATION',
+        challenge: options.challenge,
+        expiresAt: new Date(Date.now() + 5 * 60 * 1000),
+      },
+    });
+
+    return options;
+  }
+
+  async verifyAuthenticationResponseMethod(
+    username: string,
+    response: AuthenticationResponseJSON,
+  ) {
+    const user = await this.prisma.user.findUnique({
+      where: { username },
+    });
+    if (!user) {
+      throw new Error(`User ${username} not found`);
+    }
+    const authChallenge = await this.prisma.webAuthnChallenge.findFirst({
+      where: {
+        userId: user.id,
+        type: 'AUTHENTICATION',
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+    if (!authChallenge) {
+      throw new Error('No authentication challenge found');
+    }
+
+    const passkey = await this.prisma.passkey.findFirst({
+      where: {
+        userId: user.id,
+        id: response.id,
+      },
+    });
+    if (!passkey) {
+      throw new Error(
+        `Could not find passkey ${response.id} for user ${user.id}`,
+      );
+    }
+
+    const verification = await verifyAuthenticationResponse({
+      response,
+      expectedChallenge: authChallenge.challenge,
+      expectedOrigin: process.env.WEBAUTHN_ORIGIN!,
+      expectedRPID: process.env.WEBAUTHN_RPID!,
+      credential: {
+        id: passkey.id,
+        publicKey: passkey.publicKey,
+        counter: passkey.counter,
+        transports:
+          (passkey.transports as AuthenticatorTransportFuture[]) || [],
+      },
+    });
+
+    const { verified, authenticationInfo } = verification;
+    if (verified && authenticationInfo) {
+      await this.prisma.passkey.update({
+        where: { id: passkey.id },
+        data: { counter: authenticationInfo.newCounter },
+      });
+    }
+    return { verified };
   }
 }
