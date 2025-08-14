0627876f08628efb455f727c8eca2fb1b45f2df6
Lalit Rajak <lalitrajak312@gmail.com>
2025-08-15 00:06:00 +0530

    added end points for the services

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import type {
+  PublicKeyCredentialCreationOptionsJSON,
+  PublicKeyCredentialRequestOptionsJSON,
+  RegistrationResponseJSON,
+  AuthenticationResponseJSON,
+} from '@simplewebauthn/server';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register/options')
+  async generateRegistrationOptions(
+    @Body('username') username: string,
+  ): Promise<PublicKeyCredentialCreationOptionsJSON> {
+    return this.authService.generateRegistrationOptions(username);
+  }
+
+  @Post('register/verify/:userId')
+  async verifyRegistrationResponse(
+    @Param('userId') userId: string,
+    @Body() body: RegistrationResponseJSON,
+  ) {
+    return this.authService.verifyRegistrationResponse(userId, body);
+  }
+
+  @Post('authenticate/options')
+  async generateAuthenticationOptions(
+    @Body('username') username: string,
+  ): Promise<PublicKeyCredentialRequestOptionsJSON> {
+    return this.authService.generateAuthenticationOptions(username);
+  }
+
+  @Post('authenticate/verify')
+  async verifyAuthenticationResponse(
+    @Body('username') username: string,
+    @Body() body: AuthenticationResponseJSON,
+  ) {
+    return this.authService.verifyAuthenticationResponseMethod(username, body);
+  }
 }
